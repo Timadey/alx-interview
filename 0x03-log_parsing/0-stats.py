3bf70e7e5bd4dfc0587a3ff69e1824f60e031f18
Timadey <dlktimothy@gmail.com>
2023-02-17 02:16:35 +0100

    Fix pycodestyle error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,29 +3,19 @@ import sys
 import re
 
 """
-A script that reads stdin line by line and computes metrics:
-
-- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-(if the format is not this one, the line must be skipped)
-- After every 10 lines and/or a keyboard interruption (CTRL + C),
-    print these statistics from the beginning:
-    - Total file size: File size: <total size>
-    - where <total size> is the sum of all previous <file size> (see input format above)
-    - Number of lines by status code:
-        - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-        - if a status code does not appear or is not an integer, do not print anything for this status code
-        - format: <status code>: <number>
-        - status codes should be printed in ascending order
+A script that reads stdin line by line and computes metrics
 """
 
 
 def validateuser_Input(user_input: str) -> bool:
     """
     Validate the user_input to conform with the format
-    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
     """
-    pattern = re.compile(
-        r'^(\d{1,255}\.){3,3}\d{1,255} - \[.+] "GET \/projects\/260 HTTP\/1.1" (200|301|400|401|403|404|405|500) \d+$')
+    ip = r'^(\d{1,255}\.){3,3}\d{1,255} '
+    mid = r'- \[.+] "GET \/projects\/260 HTTP\/1.1" '
+    code = r'(200|301|400|401|403|404|405|500) \d+$'
+    pat = ip + mid + code
+    pattern = re.compile(pat)
     return re.match(pattern, user_input)
 
 
