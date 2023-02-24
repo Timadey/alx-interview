0d648f82bb98f4ec62fb3f73dd0a3f945c8a8f2d
Timadey <dlktimothy@gmail.com>
2023-02-24 08:56:46 +0100

    Edit implementation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,16 +18,16 @@ def validUTF8(data):
     num_bytes = 0
     for byte in data:
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            if byte >> 5 == 0b110 or byte >> 5 == 0b1110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:
+            elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7):
+            elif byte >> 7:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
             num_bytes -= 1
     return num_bytes == 0
